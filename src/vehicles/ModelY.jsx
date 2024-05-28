75a6f7c6ab691761be94f5537f07b30746f6efae
Ty Byers <tyrone@wraphx.com>
2024-05-28 15:12:12 -0400

    Final Updates

--- src/vehicles/ModelY.jsx
+++ src/vehicles/ModelY.jsx
@@ -1,38 +1,82 @@
-import React from 'react'
-import { useGLTF } from '@react-three/drei'
+
+import React, { useRef } from 'react'
 import { useSnapshot } from "valtio"
+
+import { useGLTF } from '@react-three/drei'
 import { useCustomization } from '../context/Customization';
 import { state } from '../components/ColorPicker';
 
-export function ModelY(props) {
+
+export function Darkhorse(props) {
+  const { nodes, materials } = useGLTF('models/DarkHorse.glb')
   const { overlay } = useCustomization()
   const snap1 = useSnapshot(state)
-  const { nodes, materials } = useGLTF('/models/ModelY.glb')
+
+  const canvasRef = useRef(document.createElement("canvas"));
+  const textureRef = useRef();
+  const context = useRef(canvasRef.current.getContext("2d"));
+console.log()
+
+  canvasRef.current.width = 1600;
+  canvasRef.current.height = 1600;
+
+  let ctx = context.current;
+
+  ctx.globalCompositeOperation = "hue";
+
+  const img = document.getElementById(`overlay${overlay}`);
+
+  if (img.complete) {
+    ctx.drawImage(img, 0, 0);
+    if (snap1.base.BaseMtl !== "none") {
+      if(overlay!==1){
+      ctx.fillStyle = snap1.base.BaseMtl;
+      ctx.fillRect(0, 0, 1600, 1600);
+      }
+    }
+
+
+    if (textureRef.current) {
+      textureRef.current.needsUpdate = true;
+    }
+  }
+
+  else {
+    img.onload = function () {
+      ctx.drawImage(img, 0, 0);
+      if (snap1.base.BaseMtl !== "none") {
+        if(overlay!==1){
+        ctx.fillStyle = snap1.base.BaseMtl;
+        ctx.fillRect(0, 0, 1600, 1600);
+        }
+      }
+      
+
+
+      if (textureRef.current) {
+        textureRef.current.needsUpdate = true;
+      }
+    }
+  }
+
   return (
     <group {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]} scale={0.01}>
         <group rotation={[Math.PI / 2, 0, 0]}>
           <group scale={100}>
-          <mesh geometry={nodes.Body.geometry} material={materials.BaseMtl} material-color={snap1.base.BaseMtl} visible={overlay === 1}/>
-            <mesh geometry={nodes.Body001.geometry} material={materials.Slaughter} material-color={snap1.base.BaseMtl} visible={overlay === 2}/>
-            <mesh geometry={nodes.Body002.geometry} material={materials.BatLady} material-color={snap1.base.BaseMtl} visible={overlay === 3}/>
-            <mesh geometry={nodes.Body003.geometry} material={materials.BrakeLine} material-color={snap1.base.BaseMtl} visible={overlay === 4}/>
-            <mesh geometry={nodes.Body004.geometry} material={materials.Danger} material-color={snap1.base.BaseMtl} visible={overlay === 5}/>
-            <mesh geometry={nodes.Body005.geometry} material={materials.Embers} material-color={snap1.base.BaseMtl} visible={overlay === 6}/>
-            <mesh geometry={nodes.Body006.geometry} material={materials.SpiderID} material-color={snap1.base.BaseMtl} visible={overlay === 7}/>
-            <mesh geometry={nodes.Body007.geometry} material={materials.Fluid} material-color={snap1.base.BaseMtl} visible={overlay === 8}/>
-            <mesh geometry={nodes.Body008.geometry} material={materials.HotRods} material-color={snap1.base.BaseMtl} visible={overlay === 9}/>
-            <mesh geometry={nodes.Body009.geometry} material={materials.Mayan} material-color={snap1.base.BaseMtl} visible={overlay === 10}/>
-            <mesh geometry={nodes.Body010.geometry} material={materials.SeaBreeze} material-color={snap1.base.BaseMtl} visible={overlay === 11}/>
-            <mesh geometry={nodes.Body011.geometry} material={materials.ShapeShifter} material-color={snap1.base.BaseMtl} visible={overlay === 12}/>
-            <mesh geometry={nodes.Body012.geometry} material={materials.Silk} material-color={snap1.base.BaseMtl} visible={overlay === 13}/>
-            <mesh geometry={nodes.Body013.geometry} material={materials.SnakeSkin} material-color={snap1.base.BaseMtl} visible={overlay === 14}/>
-            <mesh geometry={nodes.Body014.geometry} material={materials.Flare} material-color={snap1.base.BaseMtl} visible={overlay === 15}/>
-            <mesh geometry={nodes.Body015.geometry} material={materials.Dominator} material-color={snap1.base.BaseMtl} visible={overlay === 16}/>
-            <mesh geometry={nodes.Body016.geometry} material={materials.TopGun} material-color={snap1.base.BaseMtl} visible={overlay === 17} />
-            <mesh geometry={nodes.Body017.geometry} material={materials.Explosion} material-color={snap1.base.BaseMtl} visible={overlay === 18} />
-            <mesh geometry={nodes.Body018.geometry} material={materials.WildWest} material-color={snap1.base.BaseMtl} visible={overlay === 19} />
-            <mesh geometry={nodes.Body019.geometry} material={materials.Zombie} material-color={snap1.base.BaseMtl} visible={overlay === 20} />
+          <mesh geometry={nodes.Body.geometry}>
+        <meshStandardMaterial {...materials.BaseMtl} color={overlay==1 || overlay==13 ? (snap1.base.BaseMtl!=="none" ? snap1.base.BaseMtl : 0xffffff) : 0xffffff}>
+          <canvasTexture
+            flipY={false}
+            wrapS={1000}
+            wrapT={1000}
+            colorSpace='srgb'
+            ref={textureRef}
+            attach="map"
+            image={canvasRef.current}
+          />
+        </meshStandardMaterial>
+      </mesh>
           </group>
           <group position={[-83.884, 37.78, 163.232]} rotation={[-Math.PI / 2, 0, 2.845]} scale={204.779}>
             <mesh geometry={nodes.wheel001_chrome_0.geometry} material={materials.chrome} />
@@ -79,4 +123,4 @@ export function ModelY(props) {
   )
 }
 
-useGLTF.preload('/models/ModelY.glb')
+useGLTF.preload('/ModelY.glb')
