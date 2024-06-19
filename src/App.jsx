c136c631af6abbf21e99564b68566e4a4c19dd2f
DrakBlack69 <tybyers21@gmail.com>
2024-06-19 18:57:37 -0400

    Reversed Wrap List

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
         <Canvas shadows camera={{ position: [0, 0, 20], fov:50 }}>
           <color attach="background" args={['grey']} />
           <Suspense fallback={null}>
-          <ModelY scale={2.5} position={[0, -1.1, 0]} />
+          <ModelY scale={2.7} position={[0, -1.1, 0]} />
           </Suspense>
           <hemisphereLight intensity={0.5} />
           <ContactShadows resolution={1024} frames={1} position={[0, -1.16, 0]} scale={15} blur={0.5} opacity={0.5} far={20} />
@@ -49,7 +49,7 @@ export default function App() {
           </Environment>
           <ContactShadows position={[0, -0.8, 0.0]} opacity={0.75} scale={10} blur={2.5} far={0.8} />
           {/* <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={true} enablePan={false} /> */}
-          <OrbitControls autoRotate enablePan={false} enableZoom={true} minPolarAngle={Math.PI / 2.8} maxPolarAngle={Math.PI / 2.1} />
+          <OrbitControls enablePan={false} enableZoom={true} minPolarAngle={Math.PI / 2.8} maxPolarAngle={Math.PI / 2.1} />
         </Canvas>
         <Configurator />
       </div>
